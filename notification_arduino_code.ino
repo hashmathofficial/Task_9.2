b6bf3c708a6cd8aa84031cbafb649cd049e26ba7
hashmathofficial <41409243+hashmathofficial@users.noreply.github.com>
2018-10-15 02:31:15 +1100

    Arduino code notification
    A sample code for sending notification to user's mobile

---
+++ notification_arduino_code.ino
@@ -0,0 +1,58 @@
+#include <ESP8266WiFi.h>
+
+// PushingBox scenario DeviceId code and API
+String deviceId = "Hashmath's Fire Alarm";
+const char* logServer = "api.pushingbox.com";
+
+const char* ssid = "HashmathSmartSystems";
+const char* password = "***********";
+
+void setup() {
+  Serial.begin(74880);
+  // Sending a notification to your mobile phone
+  // function takes the message as a parameter
+  sendNotification("Fire detected!");
+}
+
+void sendNotification(String message){
+
+  Serial.println("- connecting to Home Router SID: " + String(ssid));
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println();
+  Serial.println("- succesfully connected");
+  Serial.println("- starting client");
+  
+  WiFiClient client;
+
+  Serial.println("- connecting to pushing server: " + String(logServer));
+  if (client.connect(logServer, 80)) {
+    Serial.println("- succesfully connected");
+    
+    String postStr = "devid=";
+    postStr += String(deviceId);
+    postStr += "&message_parameter=";
+    postStr += String(message);
+    postStr += "\r\n\r\n";
+    
+    Serial.println("- sending data...");
+    
+    client.print("POST /pushingbox HTTP/1.1\n");
+    client.print("Host: api.pushingbox.com\n");
+    client.print("Connection: close\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n");
+    client.print(postStr);
+  }
+  client.stop();
+  Serial.println("- stopping the client");
+}
+
+void loop() {}
